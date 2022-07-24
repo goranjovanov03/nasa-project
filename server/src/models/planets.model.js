5a35d1aa98c8f4a2a6602f0172dff10b7e1e261f
Goran <goranjovanov03@gmail.com>
2022-07-24 13:13:06 +0200

    route project

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,46 @@
+
+const { rejects } = require('assert');
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const path = require('path');
+const HabitablePlanets = [];
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+function loadPlanetsData(){
+    return new Promise((resolve,reject) => {
+        fs.createReadStream(path.join(__dirname,'..', '..','data','kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns:true,
+
+        }))
+        .on('data', (data) => {
+            if(isHabitablePlanet(data)){
+                HabitablePlanets.push(data);
+            }
+            
+        })
+        .on('error',(err)=>{
+            console.log(err);
+            reject(err);
+        })
+        .on('end',()=>{
+            console.log(`${HabitablePlanets.length} habitable planets found!`);
+            resolve();
+        });
+    });
+
+}
+
+
+
+
+module.exports = {
+    loadPlanetsData,
+    planets: HabitablePlanets
+}
